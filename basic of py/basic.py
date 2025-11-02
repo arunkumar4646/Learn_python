e0afc9e8f818dee8645bba44a193ebfb4b279d2e
Arunkumar <arun01082007@gmail.com>
2025-11-02 10:09:56 +0530

    today learn

---
+++ basic of py/basic.py
@@ -0,0 +1,22 @@
+# hashtage this comment ...
+
+'''
+This is multi line comment
+duifhusdbfusd
+sdihsaudy
+skdhuy
+
+'''
+# very simple print function ...
+print("Hello , how are you ?")
+
+# End line 
+
+print("Hello , how are you ? ",end="")
+print("I'm fine .")
+
+# Separate in the string 
+
+print("Hello","Hello","Hello","Hello",sep=" ! ")
+# The output is 
+# Hello ! Hello ! Hello ! Hello
---
+++ basic of py/datatypes.py
@@ -0,0 +1,25 @@
+# Data types
+
+# 1.Sting
+print("Hi !!")
+str="Hi Hi"
+print(str)
+
+# 2.Integer
+#whole numbers
+sum1=5+2
+sum2=6+6
+print(sum1)
+print(sum1+sum2)
+
+# 3.Float
+#decimal valuse
+sum1=5.3+2.8
+sum2=6.6+6.5
+print(sum1)
+print(sum1+sum2)
+
+#boolean
+boolean1=True
+boolean2=False
+
